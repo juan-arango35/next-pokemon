4c11a2366047843db32748751d1ebbc002eef89f
juan arango <juanarangozapata35@gmail.com>
2024-12-03 13:18:56 -0500

    agrega configuracion de dashboard

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  
+    images: {
+      domains: ['images.unsplash.com'], // Agrega aquí el dominio
+    },
+  
   /* config options here */
+
 };
 
 export default nextConfig;
---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function CounterPage() {
+  return (
+   <>
+   <h1>Counter page</h1>
+   </>
+  );
+}
