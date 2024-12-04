13a12674a8c20f93bf6c7675dcd5b169cde94864
juan arango <juanarangozapata35@gmail.com>
2024-12-04 04:55:43 -0500

    renderizado de pokemons

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,3 +1,4 @@
+import PokemonGrid from "@/app/pokemons/components/PokemonGrid";
 import { PokemonsResponse } from "@/app/pokemons/interfaces/pokemons-response";
 import { SimplePokemon } from "@/app/pokemons/interfaces/simple-pokemon";
 import Image from "next/image";
@@ -22,23 +23,9 @@ const PokemonsPAge = async () => {
 
   return (
     <div className="flex flex-col">
-      <div className="flex flex-wrap gap-10 items-center justify-center">
-        {
-            pokemons.map((item, id)=>(
-                <div>
-                   <span>{item.name}</span>
-                    <Image
-                    key={id}
-                    alt={item.name}
-                      width={100}
-                      height={100}
-                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${item.id}.svg`}
-                    />
-                </div>
-
-            ))
-        }
-      </div>
+      <span className="text-5xl my-2">Listado de Pokemons <small>Estatico</small> </span>
+      <PokemonGrid pokemons={pokemons}/>
+      
     </div>
   );
 };
