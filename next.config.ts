ce26c5b2176e913d631d04619930f4b57136f0ad
juan arango <juanarangozapata35@gmail.com>
2024-12-04 12:23:22 -0500

    agrega pagina de error y notfound

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   
     images: {
-      domains: ['images.unsplash.com'], // Agrega aquí el dominio
+      domains: ['images.unsplash.com','raw.githubusercontent.com'], // Agrega aquí el dominio
     },
   
   /* config options here */
